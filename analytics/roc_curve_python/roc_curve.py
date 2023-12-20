6f0b49bce385a04a6693eddfa642f55e9f636022
Vinay Pant <vinaypant24@gmail.com>
2023-12-20 10:21:56 +0530

    Code for Decision Trees Added

---
+++ analytics/roc_curve_python/roc_curve.py
@@ -0,0 +1,20 @@
+# Defining the function to plot the ROC curve
+def draw_roc( actual, probs ):
+    fpr, tpr, thresholds = metrics.roc_curve( actual, probs,
+                                              drop_intermediate = False )
+    auc_score = metrics.roc_auc_score( actual, probs )
+    plt.figure(figsize=(5, 5))
+    plt.plot( fpr, tpr, label='ROC curve (area = %0.2f)' % auc_score )
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate or [1 - True Negative Rate]')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+    return None
+
+# Calling the function
+draw_roc(y_train_pred_final.Churn, y_train_pred_final.Churn_Prob)
