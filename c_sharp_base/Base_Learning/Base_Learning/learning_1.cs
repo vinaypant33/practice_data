631fdebee696922fe88cc3a26179c7e32be86c04
Vinay Pant <vinaypant24@gmail.com>
2025-06-01 10:38:17 +0100

    Code Save

---
+++ c_sharp_base/Base_Learning/Base_Learning/learning_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Base_Learning
+{
+    class learning_1
+    {
+        static void Main_2(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Single line and statemnt which would be shown in the consle");
+            Console.ReadLine();
+            Console.WriteLine("Another statement which would be executed after the first statement and Enter key");
+            Console.ReadLine();
+            string name = "Vinay Pant ";
+            Console.WriteLine(name);
+            int numeral_value = 100;
+            Console.WriteLine(numeral_value);
+            Console.ReadLine();
+            Console.WriteLine("Another Statement Waiting to be Executed");
+            Console.ReadLine();
+            double numeral_another = 1.22;
+            Console.WriteLine(numeral_another);
+            Console.ReadLine();
+            bool confirmed = true;
+            Console.WriteLine("I am the statement which is being executed from the main panel  " + confirmed);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ c_sharp_base/Base_Learning/Base_Learning/learning_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Base_Learning
+{
+
+    class Learning_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am another class from the same program ");
+            Console.ReadLine();
+
+            
+        }
+    }
+
+
+}
