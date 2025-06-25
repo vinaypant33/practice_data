2a46e4f9752894b8b9c7f68730b58afc5bf941c1
Vinay Pant <vinaypant24@gmail.com>
2025-06-25 22:37:53 +0100

    Code Save Modal and Normal Windows

---
+++ c_sharp_base/Window_Learning/Window_Learning/model.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Window_Learning
+{
+    /// <summary>
+    /// Interaction logic for model.xaml
+    /// </summary>
+    public partial class model : Window
+    {
+        public model()
+        {
+            InitializeComponent();
+        }
+    }
+}
