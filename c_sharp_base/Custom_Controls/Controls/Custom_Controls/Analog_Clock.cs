2e5bfbd17fc655dc971139398cc25a02b60c5e6f
Vinay Pant <vinaypant24@gmail.com>
2025-08-01 14:46:53 +0100

    Code Save Custom Controls

---
+++ c_sharp_base/Custom_Controls/Controls/Custom_Controls/Analog_Clock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Controls.Custom_Controls
+{
+    internal class Analog_Clock : Control
+    {
+        private Line hourhand;
+        private Line minutehand;
+        private Line second_hand;
+        private DispatcherTimer timer;
+
+        static Analog_Clock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(Analog_Clock),
+                new System.Windows.FrameworkPropertyMetadata(typeof(Analog_Clock))
+            );
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            hourhand = GetTemplateChild("hour_hand") as Line;
+            minutehand = GetTemplateChild("minute_hand") as Line;
+            second_hand = GetTemplateChild("second_hand") as Line;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (s, e) => UpdateHands();
+            timer.Start();
+        }
+
+        private void UpdateHands()
+        {
+            DateTime now = DateTime.Now;
+
+            if (hourhand != null)
+                hourhand.RenderTransform = new RotateTransform((now.Hour % 12 / 12.0) * 360 + (now.Minute / 60.0) * 30, 0.5, 0.5);
+
+            if (minutehand != null)
+                minutehand.RenderTransform = new RotateTransform((now.Minute / 60.0) * 360, 0.5, 0.5);
+
+            if (second_hand != null)
+                second_hand.RenderTransform = new RotateTransform((now.Second / 60.0) * 360, 0.5, 0.5);
+        }
+    }
+}
---
+++ c_sharp_base/Custom_Controls/Custom_Controls/Custom_Controls/Analog_Clock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Custom_Controls.Custom_Controls
+{
+    internal class Analog_Clock : Control
+    {
+
+
+    }
+}
