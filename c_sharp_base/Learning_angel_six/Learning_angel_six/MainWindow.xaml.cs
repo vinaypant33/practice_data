0866ac68c3ee430be5a2ec1ee183ca438a3050a3
Vinay Pant <vinaypant24@gmail.com>
2025-10-27 16:11:37 +0000

    Code Save

---
+++ c_sharp_base/Learning_angel_six/Learning_angel_six/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Learning_angel_six
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void butotn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I am the another button for the main application");
+            this.description_text.Text = "I am another text";
+        }
+    }
+}
