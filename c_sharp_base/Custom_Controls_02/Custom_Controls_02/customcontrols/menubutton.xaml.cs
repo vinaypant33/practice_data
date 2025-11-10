85ce1113d55e7e62782bd2189bb5b742f4209f7b
Vinay Pant <vinaypant24@gmail.com>
2025-11-10 21:23:40 +0000

    Code Save WPF

---
+++ c_sharp_base/Custom_Controls_02/Custom_Controls_02/customcontrols/menubutton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Custom_Controls_02.customcontrols
+{
+    /// <summary>
+    /// Interaction logic for menubutton.xaml
+    /// </summary>
+    public partial class menubutton : UserControl
+    {
+        public menubutton()
+        {
+            InitializeComponent();
+        }
+
+        public PathGeometry Icon
+        {
+            get { return (PathGeometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using the dependency property 
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PathGeometry), typeof(menubutton));
+    }
+}
